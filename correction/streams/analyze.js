f7aa0e382b5495d1c99ae6e084d2eaddcd64952f
Quentin Tomasicchio <quentin.tomasicchio@worldline.com>
2017-11-27 14:21:21 +0100

    [QTO] Update Stream part with ES6 syntax

--- correction/streams/analyze.js
+++ correction/streams/analyze.js
@@ -1,29 +1,29 @@
-var fs = require('fs');
+const fs = require('fs');
 
 if (process.argv.length !== 3) {
   throw new Error('you need to specify input file');
 }
-var file = process.argv[2];
+const file = process.argv[2];
 
-var analysis = {
+const analysis = {
   readable: 0,
   totalSize: 0,
   nbReads: 0,
   chunkSize: 0
 };
 
-var readable = fs.createReadStream(file);
+const readable = fs.createReadStream(file);
 
-readable.on('readable', function() {
-  for (var chunk = readable.read(); chunk !== null; chunk = readable.read()) {
+readable.on('readable', () => {
+  for (let chunk = readable.read(); chunk !== null; chunk = readable.read()) {
     analysis.totalSize += chunk.length;
     analysis.nbReads++;
     analysis.chunkSize = analysis.chunkSize === 0 ? chunk.length : analysis.chunkSize;
   }
   analysis.readable++;
-}).on('error', function(err) {
-  console.error('failed to cat file ' + file + ':', err);
-}).on('end', function() {
+}).on('error', (err) => {
+  console.error('failed to analyse file ' + file + ':', err);
+}).on('end', () => {
   console.log('received ' + analysis.readable + ' events and read ' +
     analysis.nbReads + ' chunks (average ' +
     analysis.chunkSize + ') for ' +
--- correction/streams/encoder.js
+++ correction/streams/encoder.js
@@ -1,19 +1,22 @@
-var fs = require('fs');
+const fs = require('fs');
 if (process.argv.length !== 4) {
   throw new Error('you need to specify input and output file');
 }
-var inputFile = process.argv[2];
-var outputFile = process.argv[3];
+const inputFile = process.argv[2];
+const outputFile = process.argv[3];
 
-var input = fs.createReadStream(inputFile);
-var output = fs.createWriteStream(outputFile);
+const input = fs.createReadStream(inputFile);
+const output = fs.createWriteStream(outputFile);
 
-input.on('readable', function() {
-  var data = input.read();
-  output.write(data.toString('base64'));
-}).on('end', function() {
+input.on('readable', () => {
+  const data = input.read();
+
+  if(data !== null) {
+    output.write(data.toString('base64'));
+  }
+}).on('end', () => {
   output.end();
   console.log('file encoded into', outputFile);
-}).on('error', function(err) {
+}).on('error', (err) => {
   console.error('failed to encode file:', err.message);
 });
