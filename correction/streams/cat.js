7a8e105bfdbab59065a72ec95ae3958d3cfbd440
Quentin Tomasicchio <quentin.tomasicchio@worldline.com>
2017-11-27 14:21:21 +0100

    [QTO] Update Stream part with ES6 syntax

--- correction/streams/cat.js
+++ correction/streams/cat.js
@@ -1,8 +1,10 @@
-var fs = require('fs');
+const fs = require('fs');
+
 if (process.argv.length !== 3) {
   throw new Error('you need to specify input file');
 }
-var file = process.argv[2];
-fs.createReadStream(file).on('error', function(err) {
+
+const file = process.argv[2];
+fs.createReadStream(file).on('error', (err) => {
   console.error('failed to cat file ' + file + ':', err);
 }).pipe(process.stdout);
