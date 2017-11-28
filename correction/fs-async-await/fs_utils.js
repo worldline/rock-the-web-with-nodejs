238f293fadf644b3b829c4c6e0f4c45bbd3fc685
Yann Odeyer <yann.odeyer@worldline.com>
2017-11-28 14:38:53 +0100

    Various fixes

--- correction/fs-async-await/fs_utils.js
+++ correction/fs-async-await/fs_utils.js
@@ -8,10 +8,10 @@ const stat = util.promisify(fs.stat);
 /**
  * Built an array of item names contained in a directory
  * @param {String} path - inspected path
- * @param {getDirContent-CB} done - completion callback
+ *
  * @return {Promise<String[]>} - directory content.
  */
-exports.getDirContent = async function(path, done) {
+exports.getDirContent = async function(path) {
   // Rely on built-in function
   const content = await readdir(path);
   return content.map((item) => {
@@ -22,11 +22,8 @@ exports.getDirContent = async function(path, done) {
 /**
  * Returns details on each items contained into a given directory
  * @param {String} path - inspected path
- * @param {getDirStat-CB} done - completion callback, invoked with parameters:
  *
- * @callback getDirStat-CB
- * @param {Error} err - an optionnal error if path cannot be read as directory
- * @param {Object[]} result - details of each element contained
+ * @return {Promise<Object[]>} result - details of each element contained
  * @param {String} result.path - inspected item path
  * @param {String} result.status - file, directory or unknown nature of the item
  * @param {Number} result.size - item size in octets
